47ee0a631f3929d1849245c0516c03d9cb427a7b
Santiago Ortiz Diaz <96148705+ortizdiaz18@users.noreply.github.com>
2023-07-04 20:28:14 -0500

    Update App.jsx
    Se actualiza la URL para consumir server

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,8 @@ const App = () => {
       const fetchData = async () => {
         try {
           const response = await fetch(
-            `https://open-rat-production.up.railway.app/api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
+            
+            `https://geoserver-production.up.railway.app//api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
           );
           const data = await response.json();
           setCoordinates(data);
