b6aec1ac99d0b69138b59642d78ea27b3d0d5ba7
ortizdiaz18 <96148705+ortizdiaz18@users.noreply.github.com>
2023-07-05 23:01:40 -0500

    Se sube con la URL del server online

--- src/App.jsx
+++ src/App.jsx
@@ -124,8 +124,8 @@ const App = () => {
       const fetchData = async () => {
         try {
           const response = await fetch(
-            //  `https://geoserver-production.up.railway.app/api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
-            `http://localhost:3000/api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
+             `https://geoserver-production.up.railway.app/api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
+            // `http://localhost:3000/api/v1/cobertura?latitud=${latitud}&longitud=${longitud}`
           );
           const data = await response.json();
           setCoordinates(data);
