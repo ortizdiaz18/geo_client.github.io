059207be6a82121cc6eb81ef47039c030b9e9204
ortizdiaz18 <96148705+ortizdiaz18@users.noreply.github.com>
2024-05-28 08:45:56 -0500

    Se actualiza ek viete.config

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { defineConfig, loadEnv, preprocessCSS } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig((command, mode)=>{
-  const env = loadEnv(mode, process.cwd(),'')
-  return{
-    plugins: [react()],    
-    define:{
-      __APP_ENV__:env.APP_ENV
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    // base: "https://ortizdiaz18.github.io/geo_client",
+    define: {
+      __APP_ENV__: env.APP_ENV,
     },
-    preview:{
-      port:process.env.PORT
-    } 
-   
-  }
-  
+    preview: {
+      port: process.env.PORT,
+    },
+  };
 });
